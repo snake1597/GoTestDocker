2abc0a925b483bd2ebf0ea9d2cd5586267137218
Arthur She <arthur@aihavefun.com>
2022-03-29 14:28:27 +0800

    feat: update a simple http server for test

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Status string `json:"status"`
+}
 
 func main() {
-	fmt.Println("Hellow Docker")
+	http.HandleFunc("/heartbeat", heartbeat)
+	http.ListenAndServe(":8080", nil)
+}
+
+func heartbeat(w http.ResponseWriter, r *http.Request) {
+	m := Response{"success"}
+	json, _ := json.Marshal(m)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
+	return
 }
